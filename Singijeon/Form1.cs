068d24dcdb2f5945418518ecfef0792acf0123e0
segara <bluestin85@gmail.com>
2020-04-21 16:00:27 +0900

    test

--- Singijeon/Form1.cs
+++ Singijeon/Form1.cs
@@ -3056,12 +3056,11 @@ namespace Singijeon
                     int curLastQnt = tradeItem.curQnt;
                     tradeItem.curQnt += addQnt;
 
-                    long PriceAverage = (long)((float)((curLastQnt * tradeItem.buyingPrice) + (priceUpdate * addQnt)) / tradeItem.curQnt);
-                    
-                    tradeItem.buyingPrice = PriceAverage;
+                    //long PriceAverage = (long)((float)((curLastQnt * tradeItem.buyingPrice) + (priceUpdate * addQnt)) / tradeItem.curQnt);
+                    //coreEngine.SaveItemLogMessage(tradeItem.itemCode, "평단가 : " + PriceAverage);
+                    //tradeItem.buyingPrice = PriceAverage;
                     tradeItem.SetCompleteBuying(buyComplete);
 
-                    coreEngine.SaveItemLogMessage(tradeItem.itemCode, "평단가 : "+ PriceAverage);
                     coreEngine.SaveItemLogMessage(tradeItem.itemCode, "보유량 : " + tradeItem.curQnt);
 
                     if (buyComplete)
