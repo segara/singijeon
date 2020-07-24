940a5e968d4abfdc57395f98a819a5671ea28a49
unknown <segara@naver.com>
2020-07-24 09:39:52 +0900

    1.0 commit

--- Singijeon/Form2.Designer.cs
+++ Singijeon/Form2.Designer.cs
@@ -35,7 +35,6 @@
             this.d2Asset_label = new System.Windows.Forms.Label();
             this.estimatedAsset_label = new System.Windows.Forms.Label();
             this.investment_label = new System.Windows.Forms.Label();
-            this.profit_label = new System.Windows.Forms.Label();
             this.profitRate_label = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
@@ -43,6 +42,7 @@
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
+            this.profit_label = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.martin_lose_txt = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -65,6 +65,17 @@
             this.warningLogListBox = new System.Windows.Forms.ListBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.errorListBox = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.b_takeProfitUpdown = new System.Windows.Forms.NumericUpDown();
+            this.bss_curQnt = new System.Windows.Forms.Label();
+            this.label42 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.groupBox6.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -74,6 +85,10 @@
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.b_takeProfitUpdown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox6
@@ -181,19 +196,6 @@
             this.investment_label.Text = "당일투자원금";
             this.investment_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // profit_label
-            // 
-            this.profit_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.profit_label.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.profit_label.Location = new System.Drawing.Point(169, 124);
-            this.profit_label.Name = "profit_label";
-            this.profit_label.Size = new System.Drawing.Size(161, 31);
-            this.profit_label.TabIndex = 10;
-            this.profit_label.Text = "당일투자손익";
-            this.profit_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // profitRate_label
             // 
             this.profitRate_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -284,6 +286,19 @@
             this.label15.TabIndex = 5;
             this.label15.Text = "당일손익률";
             this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // profit_label
+            // 
+            this.profit_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.profit_label.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.profit_label.Location = new System.Drawing.Point(169, 124);
+            this.profit_label.Name = "profit_label";
+            this.profit_label.Size = new System.Drawing.Size(161, 31);
+            this.profit_label.TabIndex = 10;
+            this.profit_label.Text = "당일투자손익";
+            this.profit_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
             // 
@@ -533,11 +548,129 @@
             this.errorListBox.Size = new System.Drawing.Size(867, 304);
             this.errorListBox.TabIndex = 1;
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.numericUpDown2);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.numericUpDown1);
+            this.groupBox3.Controls.Add(this.bss_curQnt);
+            this.groupBox3.Controls.Add(this.label42);
+            this.groupBox3.Controls.Add(this.label17);
+            this.groupBox3.Controls.Add(this.b_takeProfitUpdown);
+            this.groupBox3.Location = new System.Drawing.Point(680, 11);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(213, 260);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "승률-손익 계산기";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(17, 34);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(53, 12);
+            this.label17.TabIndex = 5;
+            this.label17.Text = "추정승률";
+            // 
+            // b_takeProfitUpdown
+            // 
+            this.b_takeProfitUpdown.DecimalPlaces = 2;
+            this.b_takeProfitUpdown.Location = new System.Drawing.Point(93, 31);
+            this.b_takeProfitUpdown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.b_takeProfitUpdown.Name = "b_takeProfitUpdown";
+            this.b_takeProfitUpdown.Size = new System.Drawing.Size(105, 21);
+            this.b_takeProfitUpdown.TabIndex = 4;
+            // 
+            // bss_curQnt
+            // 
+            this.bss_curQnt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bss_curQnt.AutoSize = true;
+            this.bss_curQnt.Location = new System.Drawing.Point(102, 63);
+            this.bss_curQnt.Name = "bss_curQnt";
+            this.bss_curQnt.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.bss_curQnt.Size = new System.Drawing.Size(59, 12);
+            this.bss_curQnt.TabIndex = 12;
+            this.bss_curQnt.Text = "999999999";
+            this.bss_curQnt.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label42
+            // 
+            this.label42.AutoSize = true;
+            this.label42.Location = new System.Drawing.Point(19, 63);
+            this.label42.Name = "label42";
+            this.label42.Size = new System.Drawing.Size(41, 12);
+            this.label42.TabIndex = 11;
+            this.label42.Text = "손익비";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 12);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "손절%";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(93, 90);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(105, 21);
+            this.numericUpDown1.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(102, 123);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(59, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "999999999";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 123);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 12);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "최소익절%";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 148);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 12);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "추정승률";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Location = new System.Drawing.Point(93, 145);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(105, 21);
+            this.numericUpDown2.TabIndex = 17;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(905, 624);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -554,6 +687,11 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.b_takeProfitUpdown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -596,5 +734,16 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.ListBox errorListBox;
         private System.Windows.Forms.Button accountRefreshBtn;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.NumericUpDown b_takeProfitUpdown;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label bss_curQnt;
+        private System.Windows.Forms.Label label42;
     }
 }
